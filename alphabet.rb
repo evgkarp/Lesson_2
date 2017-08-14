e01cb5d004e33b22fef26515e5e1a48c7253198c
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-08-14 21:19:23 +0700

    fix_v2

--- alphabet.rb
+++ alphabet.rb
@@ -1,6 +1,6 @@
 vowel = {}
 vowel_arr = %w( a e i o u )
-alphabet = ( "a".."z" ).to_a
+alphabet = ( "a".."z" )
 
 alphabet.each_with_index do |letter, i|
  vowel.store( letter, (i + 1) ) if vowel_arr.include?(letter)
--- fibonacci.rb
+++ fibonacci.rb
@@ -1,11 +1,9 @@
 arr = [ 1, 1 ]
 
-i = 2
 arr_add = 0
 while arr_add < 100
-  arr_add = ( arr[i-1] + arr[i-2] )
+  arr_add = arr[-1] + arr[-2]
   arr << arr_add if arr_add < 100
-  i += 1
 end
 
 puts arr
