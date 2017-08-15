555065cc4eae60f8fc5af18f7d51048e85d3ee96
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-08-15 19:32:53 +0700

    fix_v5

--- serial_number.rb
+++ serial_number.rb
@@ -2,6 +2,9 @@ print "Введите число, месяц и год (dd, mm, yyyy): "
 date = gets.split(',').map(&:to_i)
 
 months = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+months[1] = 29 if (date[2] % 4 == 0) && 
+                  (date[2] % 100 != 0) || 
+                  (date[2] % 400 == 0)
 
 i = 0
 serial_number = 0
@@ -10,7 +13,5 @@ while i < date[1] - 1
   i += 1
 end
 serial_number += date[0]
-
-serial_number += 1 if (date[2] % 4 == 0) && (date[2] % 100 != 0) || (date[2] % 400 == 0)
 
 puts "Порядковый номер даты: #{serial_number}"
